82ec81513aa16a677d6eb8d5a5c5f885ed5e1924
himasnhu018 <himanshukrdev007@gmail.com>
2025-07-12 16:40:56 +0530

    frontend

--- backend/routes/ratingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middlewares/authMiddleware');
-const { submitFeedback, getUserRatings } = require('../controllers/ratingController');
-
-router.post('/:swapId', protect, submitFeedback);
-router.get('/user/:userId', getUserRatings);
-
-module.exports = router;
